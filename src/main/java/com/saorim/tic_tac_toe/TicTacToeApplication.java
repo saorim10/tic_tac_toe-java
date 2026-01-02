eb35b56815920065cb06b1465b6e80ee148de0bf
Saorim <meom@gft.com>
2026-01-02 18:46:13 -0300

    adjust main class

--- src/main/java/com/saorim/tic_tac_toe/TicTacToeApplication.java
+++ src/main/java/com/saorim/tic_tac_toe/TicTacToeApplication.java
@@ -1,13 +1,20 @@
 package com.saorim.tic_tac_toe;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.saorim.tic_tac_toe.controller.GameController;
+import com.saorim.tic_tac_toe.service.GameService;
+import com.saorim.tic_tac_toe.view.ConsoleView;
 
 @SpringBootApplication
 public class TicTacToeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TicTacToeApplication.class, args);
+		GameService gameService = new GameService();
+        ConsoleView view = new ConsoleView();
+        GameController controller = new GameController(gameService, view);
+        
+        controller.run();
 	}
 
 }
